7da874c99e923098f4ae7057ae39682551c7dcf3
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-06-12 11:08:25 +0200

    a

--- dind/create-docker-image.sh
+++ dind/create-docker-image.sh
@@ -2,5 +2,7 @@
 
 cd /gitsource/src3
 cp ./Dockerfile.build ./Dockerfile
-docker build .
-docker login "chgeuerregistry1.azurecr.io" --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}"
+
+docker login "${DOCKER_REGISTRY}" --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}"
+docker build . -t "${DOCKER_REGISTRY}/chgeuer/elixir:1.4.4"
+docker push       "${DOCKER_REGISTRY}/chgeuer/elixir:1.4.4"
