28c32c1dc68aac472e157c5b8dc5078e0260c294
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-06-12 11:43:48 +0200

    a

--- dind/create-docker-image.sh
+++ dind/create-docker-image.sh
@@ -2,6 +2,10 @@
 
 echo "Running create-docker-image.sh"
 
+DOCKER_REGISTRY=$(echo $DOCKER_SECRET_CFG | jq -r '. | keys[0]')
+DOCKER_USERNAME=$(echo $DOCKER_SECRET_CFG | jq -r '.[. | keys[0]].username')
+DOCKER_PASSWORD=$(echo $DOCKER_SECRET_CFG | jq -r '.[. | keys[0]].password')
+
 echo "DOCKER_REGISTRY ${DOCKER_REGISTRY}"
 echo "DOCKER_USERNAME ${DOCKER_USERNAME}"
 echo "DOCKER_PASSWORD ${DOCKER_PASSWORD}"
